0d9e1f0ac29cf9479656442f323ffa1c172609bc
Ubuntu <ubuntu@localhost.localdomain>
2026-07-10 07:49:22 +0800

    Fix: matikan shrinkResources agar build release sukses

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,10 +29,7 @@ android {
         release {
             signingConfig = signingConfigs.getByName("debug")
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            isShrinkResources = false
         }
     }
 }
